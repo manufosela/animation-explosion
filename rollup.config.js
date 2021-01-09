ba0a75c876c4c08dd774a324a7becf38efd2c274
Manuel Juan Fosela Águila <manufosela@gmail.com>
2021-01-09 22:19:39 +0100

    updated demo and build tasks

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,39 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import copy from 'rollup-plugin-copy';
 import { terser } from 'rollup-plugin-terser';
 
 export default {
   preserveSymlinks: true,
   input: [ 'animation-explosion.js' ],
   output: {
-    file: 'build/animation-explosion.min.js',
+    dir: './dist/js',
     format: 'es',
-    sourcemap: true
+    sourcemap: true,
   },
   plugins: [
     resolve(),
     babel(),
-    terser()
-  ]
+    terser({
+      output: {
+        comments: false,
+      },
+    }),
+    copy({
+      targets: [{
+        src: './animation-explosion.js',
+        dest: './dist/js',
+      },{
+        src: './demo/css/styles.css',
+        dest: './dist/css',
+      },{
+        src: './demo/img/*',
+        dest: './dist/img',
+      },{
+        src: './demo/index.html',
+        dest: './dist',
+        transform: (contents) => contents.toString().replace('<script type="module" src="..', '<script type="module" src="js')
+      }],
+    }),
+  ],
 };
